f38a1f37211028419ab15df0f4e2842150eb2577
SLPMods <38368798+SLPMods@users.noreply.github.com>
2018-04-14 06:24:00 +0300

    Create SLPDevPoints.js

---
+++ SLPDevPoints.js
@@ -0,0 +1,3 @@
+$(".head.nosel.bg.fl").html(`<label class="label fl hand fa fa-info title" style="margin:2px;margin-right: 2px;">&nbsp;معلومات الشخص</label>
+        <label style="padding:8px;" onclick="$(this).parent().parent().remove();" class="btn minix btn-danger pphide fr border fa fa-close phide">&nbsp;&nbsp;</label
+        `);
